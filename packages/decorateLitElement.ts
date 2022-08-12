9d900aa69a9c2b18b7d0abaf4d7a95ce87629e15
arshia11d <arshia11d@outlook.com>
2022-08-12 19:48:30 +0200

    refactor: Support for handling inheritance  for decorateLitElement

--- packages/decorateLitElement.ts
+++ packages/decorateLitElement.ts
@@ -1,25 +1,43 @@
 /* eslint-disable no-prototype-builtins */
 import { LitElement } from 'lit'
 
-export const decorateLitElement = <T>({ constructorPropertyName, prototype, initialValue, lifecycleHooks }: {
-	constructorPropertyName: string
+type InheritanceHandler<T> = (own: T, inherited?: T) => T
+
+const defaultInheritanceHandlersByType = new Map<Constructor<unknown>, InheritanceHandler<any>>([
+	[Array, (own: Array<unknown>, inherited?: Array<unknown>) => [...(inherited ?? []), ...own]],
+	[Set, (own: Set<unknown>, inherited?: Set<unknown>) => new Set([...(inherited ?? []), ...own])],
+	[Map, (own: Map<unknown, unknown>, inherited?: Map<unknown, unknown>) => new Map([...(inherited ?? []), ...own]) ],
+])
+
+const defaultInheritanceHandler: InheritanceHandler<any> = (own, inherited) => inherited ? Object.assign(own, inherited) : own
+
+function getDefaultInheritanceHandler<T>(value: T) {
+	const type = 'constructor' in value ? (value as any).constructor : undefined
+	return (defaultInheritanceHandlersByType.get(type) ?? defaultInheritanceHandler) as InheritanceHandler<T>
+}
+
+export const decorateLitElement = <T>({ constructorPropertyKey, prototype, initialValue, lifecycleHooks, inheritanceHandler }: {
+	constructorPropertyKey: symbol | string
 	prototype: LitElement
 	initialValue: T
-	lifecycleHooks: Map<string, (this: LitElement, data: T, ...args: Array<any>) => any>
+	inheritanceHandler?: InheritanceHandler<T>
+	lifecycleHooks?: Map<string, (this: LitElement, data: T, ...args: Array<any>) => any>
 }) => {
 	const p = prototype as any
 	const constructor = prototype.constructor as any
-	const existingValue = Object.getOwnPropertyDescriptor(constructor, constructorPropertyName)?.value as T | undefined
+	const existingValue = Object.getOwnPropertyDescriptor(constructor, constructorPropertyKey)?.value as T | undefined
 
 	if (existingValue) {
 		return existingValue
 	}
 
-	const inheritedValue = constructor[constructorPropertyName]
-	const value = inheritedValue ? Object.assign<T, T>(initialValue, inheritedValue) : initialValue
-	Object.defineProperty(constructor, constructorPropertyName, { value })
+	const inherit = inheritanceHandler ?? getDefaultInheritanceHandler(initialValue)
 
-	for (const [lifecycleName, lifecycleFunction] of lifecycleHooks) {
+	const inheritedValue = constructor[constructorPropertyKey]
+	const value = inherit(initialValue, inheritedValue)
+	Object.defineProperty(constructor, constructorPropertyKey, { value })
+
+	for (const [lifecycleName, lifecycleFunction] of lifecycleHooks ?? []) {
 		const originalFunction = p[lifecycleName]
 		p[lifecycleName] = function (this: LitElement, ...args: Array<any>) {
 			originalFunction.call(this, ...args)
