2586c2069914cf1560ecab2bb22cf0c1af948845
arshia11d <arshia11d@outlook.com>
2023-02-02 06:20:23 +0100

    infra: Add testing infrastructure

---
+++ src/eventListener/eventListener.test.ts
@@ -0,0 +1,28 @@
+import { eventListener, Component, component } from '../index.js'
+import { ComponentTestFixture } from '../../test/ComponentTestFixture.js'
+
+describe('@eventListener()', () => {
+	describe('used as method', () => {
+		@component('lit-test-event-listener-used-as-method')
+		class EventListenerUsedAsMethod extends Component {
+			readonly fakeCall = jasmine.createSpy('fakeCall')
+			handlerThis!: this
+			handlerEvent!: Event
+
+			@eventListener('click')
+			protected handleClick(e: Event) {
+				this.fakeCall()
+				this.handlerEvent = e
+				this.handlerThis = this
+			}
+		}
+
+		const fixture = new ComponentTestFixture(() => new EventListenerUsedAsMethod())
+		const event = new PointerEvent('click')
+		beforeEach(() => fixture.component.dispatchEvent(event))
+
+		it('calls the method', () => expect(fixture.component.fakeCall).toHaveBeenCalled())
+		it('bounds the method to the component', () => expect(fixture.component.handlerThis).toBe(fixture.component))
+		it('passes the event as the first argument', () => expect(fixture.component.handlerEvent).toBe(event))
+	})
+})
