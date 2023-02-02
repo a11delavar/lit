93f0d3fb5270346ed640de2d8957c4a3279da854
arshia11d <arshia11d@outlook.com>
2023-02-02 06:29:34 +0100

    Add more tests

--- src/eventListener/eventListener.test.ts
+++ src/eventListener/eventListener.test.ts
@@ -25,4 +25,28 @@ describe('@eventListener()', () => {
 		it('bounds the method to the component', () => expect(fixture.component.handlerThis).toBe(fixture.component))
 		it('passes the event as the first argument', () => expect(fixture.component.handlerEvent).toBe(event))
 	})
+
+	describe('used as arrow function', () => {
+		@component('lit-test-event-listener-used-as-arrow-function')
+		class EventListenerUsedAsArrowFunction extends Component {
+			readonly fakeCall = jasmine.createSpy('fakeCall')
+			handlerThis!: this
+			handlerEvent!: Event
+
+			@eventListener('click')
+			protected handleClick = (e: Event) => {
+					this.fakeCall()
+					this.handlerEvent = e
+					this.handlerThis = this
+				}
+		}
+
+		const fixture = new ComponentTestFixture(() => new EventListenerUsedAsArrowFunction())
+		const event = new PointerEvent('click')
+		beforeEach(() => fixture.component.dispatchEvent(event))
+
+		it('calls the method', () => expect(fixture.component.fakeCall).toHaveBeenCalled())
+		it('bounds the method to the component', () => expect(fixture.component.handlerThis).toBe(fixture.component))
+		it('passes the event as the first argument', () => expect(fixture.component.handlerEvent).toBe(event))
+	})
 })
