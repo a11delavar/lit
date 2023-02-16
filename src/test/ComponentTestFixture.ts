dc03f99a6e692934b20b4b9d31d67595e954b1d0
arshia11d <arshia11d@outlook.com>
2023-02-16 17:40:10 +0100

    feat(test): Add ability to pass tag-name instead of initializer function

--- src/test/ComponentTestFixture.ts
+++ src/test/ComponentTestFixture.ts
@@ -2,14 +2,17 @@ export class ComponentTestFixture<TComponent extends import('lit').LitElement> {
 	private _component?: TComponent
 	get component() { return this._component! }
 
-	constructor(private readonly constructComponent: () => TComponent) {
+	constructor(private readonly constructComponentOrTagName: string | (() => TComponent)) {
 		beforeEach(() => this.initialize())
 		afterEach(() => this._component?.remove())
 	}
 
 	async initialize() {
 		this._component?.remove()
-		this._component = this.constructComponent()
+		const constructComponent = typeof this.constructComponentOrTagName === 'string'
+			? () => document.createElement(this.constructComponentOrTagName as string) as TComponent
+			: this.constructComponentOrTagName
+		this._component = constructComponent()
 		document.body.appendChild(this._component)
 		await this._component.updateComplete
 		return this._component
