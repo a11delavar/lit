b4ee013c69660b0e44caf9961e74a5415502c64d
arshia11d <arshia11d@outlook.com>
2023-05-15 14:37:35 +0200

    test(Component): Add tests for the Component class

---
+++ src/Component/Component.test.ts
@@ -0,0 +1,41 @@
+import { html } from 'lit'
+import { Component } from '../Component/index.js'
+import { ComponentTestFixture } from '../test/ComponentTestFixture.js'
+
+class TestComponent extends Component {
+	static template = html`<div></div>`
+	override get template() { return TestComponent.template }
+}
+customElements.define('test-component', TestComponent)
+
+describe('Controller', () => {
+	const fixture = new ComponentTestFixture<TestComponent>('test-component')
+
+	it('should tunnel firstUpdated to initialized', async () => {
+		spyOn(Component.prototype as any, 'initialized')
+
+		await fixture.initialize()
+
+		expect((fixture.component as any).initialized).toHaveBeenCalledOnceWith()
+	})
+
+	it('should tunnel connectedCallback to connected', () => {
+		spyOn(fixture.component as any, 'connected')
+
+		fixture.component.connectedCallback()
+
+		expect((fixture.component as any).connected).toHaveBeenCalledOnceWith()
+	})
+
+	it('should tunnel disconnectedCallback to disconnected', () => {
+		spyOn(fixture.component as any, 'disconnected')
+
+		fixture.component.disconnectedCallback()
+
+		expect((fixture.component as any).disconnected).toHaveBeenCalledOnceWith()
+	})
+
+	it('should tunnel template to render()', () => {
+		expect((fixture.component as any).render()).toBe(TestComponent.template)
+	})
+})
