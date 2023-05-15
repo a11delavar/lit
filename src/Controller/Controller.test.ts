14f306d032b24967870d4dd78bbfc6453af7eecb
arshia11d <arshia11d@outlook.com>
2023-05-15 14:26:59 +0200

    test: Add tests for Controller class

---
+++ src/Controller/Controller.test.ts
@@ -0,0 +1,29 @@
+import { ReactiveController } from 'lit'
+import { Controller } from './Controller.js'
+import { Component } from '../Component/index.js'
+import { ComponentTestFixture } from '../test/ComponentTestFixture.js'
+
+class ControllerTestComponent extends Component {
+	readonly controllers = new Set<ReactiveController>()
+
+	override addController(controller: ReactiveController) {
+		this.controllers.add(controller)
+		super.addController(controller)
+	}
+}
+customElements.define('controller-test-component', ControllerTestComponent)
+
+class TestController extends Controller { }
+
+describe('Controller', () => {
+	const fixture = new ComponentTestFixture<ControllerTestComponent>('controller-test-component')
+
+	it('should add itself as a controller', () => {
+		expect(fixture.component.controllers.size).toBe(0)
+
+		const controller = new TestController(fixture.component)
+
+		expect(fixture.component.controllers.size).toBe(1)
+		expect(fixture.component.controllers.has(controller)).toBeTrue()
+	})
+})
