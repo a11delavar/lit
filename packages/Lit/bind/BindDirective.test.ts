fa7d830cc8a8a5929d107f41c29f5b133dc85320
arshia11d <arshia11d@outlook.com>
2023-10-16 02:03:20 +0200

    Refactor tests

--- packages/Lit/bind/BindDirective.test.ts
+++ packages/Lit/bind/BindDirective.test.ts
@@ -3,7 +3,7 @@ import '../index.js'
 import { event, Component, html, property, state, query, staticHtml, literal, unsafeStatic, TemplateResult, StaticValue } from '../index'
 import { bind } from './BindDirective.js'
 
-function expectBindingToWork<T>(parameters: {
+function expectBindToPass<T>(parameters: {
 	initialValue: T
 	updatedValue: T
 	// eslint-disable-next-line
@@ -35,6 +35,52 @@ function expectBindingToWork<T>(parameters: {
 	customElements.define(bindableComponentTagName, TestBindableComponent)
 	const tag = literal`${unsafeStatic(bindableComponentTagName)}`
 
+	interface TestBinderComponent extends Component {
+		readonly bindableComponent: TestBindableComponent
+	}
+
+	const expectBindingToPass = (parameters: {
+		fixture: ComponentTestFixture<TestBinderComponent>
+		property: string
+		keyPath?: string
+	}) => {
+		const fixture = parameters.fixture
+		const property = parameters.property
+		const keyPath = !parameters.keyPath ? property : `${property}.${parameters.keyPath}`
+
+		it('should initialize from source', () => {
+			expect(fixture.component.bindableComponent.value).toBe(initialValue)
+		})
+
+		it('should bind from source', async () => {
+			setValueByKeyPath(fixture.component as any, keyPath, updatedValue)
+			fixture.component.requestUpdate(property)
+			await fixture.updateComplete
+			expect(fixture.component.bindableComponent.value).toBe(updatedValue)
+		})
+
+		it('should update the source when dispatching associated event', async () => {
+			fixture.component.bindableComponent.change.dispatch(updatedValue)
+			await fixture.updateComplete
+			expect(getValueByKeyPath(fixture.component as any, keyPath)).toBe(updatedValue)
+		})
+
+		it('should call requestUpdate with the property key', async () => {
+			spyOn(fixture.component, 'requestUpdate')
+			fixture.component.bindableComponent.change.dispatch(updatedValue)
+			await fixture.updateComplete
+			expect(fixture.component.requestUpdate).toHaveBeenCalledWith(property)
+		})
+
+		it('should remove the event listener when disconnected', () => {
+			expect(fixture.component.bindableComponent.registeredEventsCount).toBe(1)
+
+			fixture.component.remove()
+
+			expect(fixture.component.bindableComponent.registeredEventsCount).toBe(0)
+		})
+	}
+
 	describe('direct binding', () => {
 		class TestDirectBinderComponent extends Component {
 			@state() value = initialValue
@@ -49,36 +95,7 @@ function expectBindingToWork<T>(parameters: {
 
 		const fixture = new ComponentTestFixture(() => new TestDirectBinderComponent)
 
-		it('should initialize from source', () => {
-			expect(fixture.component.bindableComponent.value).toBe(initialValue)
-		})
-
-		it('should bind from source', async () => {
-			fixture.component.value = updatedValue
-			await fixture.updateComplete
-			expect(fixture.component.bindableComponent.value).toBe(updatedValue)
-		})
-
-		it('should update the source when dispatching associated event', async () => {
-			fixture.component.bindableComponent.change.dispatch(updatedValue)
-			await fixture.updateComplete
-			expect(fixture.component.value).toBe(updatedValue)
-		})
-
-		it('should call requestUpdate with the property key', async () => {
-			spyOn(fixture.component, 'requestUpdate')
-			fixture.component.bindableComponent.change.dispatch(updatedValue)
-			await fixture.updateComplete
-			expect(fixture.component.requestUpdate).toHaveBeenCalledWith('value')
-		})
-
-		it('should remove the event listener when disconnected', () => {
-			expect(fixture.component.bindableComponent.registeredEventsCount).toBe(1)
-
-			fixture.component.remove()
-
-			expect(fixture.component.bindableComponent.registeredEventsCount).toBe(0)
-		})
+		expectBindingToPass({ fixture, property: 'value' })
 	})
 
 	describe('deep binding', () => {
@@ -96,49 +113,20 @@ function expectBindingToWork<T>(parameters: {
 
 		const fixture = new ComponentTestFixture(() => new TestDeepBinderComponent)
 
-		it('should initialize from source', () => {
-			expect(fixture.component.bindableComponent.value).toBe(initialValue)
-		})
-
-		it('should bind from source', async () => {
-			fixture.component.deep = { object: { value: updatedValue } }
-			await fixture.updateComplete
-			expect(fixture.component.bindableComponent.value).toBe(updatedValue)
-		})
-
 		it('should update on explicit requestUpdate', async () => {
 			fixture.component.deep.object.value = updatedValue
 			await fixture.update()
 			expect(fixture.component.bindableComponent.value).toBe(updatedValue)
 		})
 
-		it('should update the source when dispatching associated event', async () => {
-			fixture.component.bindableComponent.change.dispatch(updatedValue)
-			await fixture.updateComplete
-			expect(fixture.component.deep.object.value).toBe(updatedValue)
-		})
-
-		it('should call requestUpdate with the property key', async () => {
-			spyOn(fixture.component, 'requestUpdate')
-			fixture.component.bindableComponent.change.dispatch(updatedValue)
-			await fixture.updateComplete
-			expect(fixture.component.requestUpdate).toHaveBeenCalledWith('deep')
-		})
-
-		it('should remove the event listener when disconnected', () => {
-			expect(fixture.component.bindableComponent.registeredEventsCount).toBe(1)
-
-			fixture.component.remove()
-
-			expect(fixture.component.bindableComponent.registeredEventsCount).toBe(0)
-		})
+		expectBindingToPass({ fixture, property: 'deep', keyPath: 'object.value' })
 	})
 }
 
 
 describe('BindDirective', () => {
 	describe('attribute', () => {
-		expectBindingToWork({
+		expectBindToPass({
 			initialValue: 'initial',
 			updatedValue: 'updated',
 			converterType: String,
@@ -147,7 +135,7 @@ describe('BindDirective', () => {
 	})
 
 	describe('boolean attribute', () => {
-		expectBindingToWork({
+		expectBindToPass({
 			initialValue: undefined,
 			updatedValue: true,
 			converterType: Boolean,
@@ -156,7 +144,7 @@ describe('BindDirective', () => {
 	})
 
 	describe('property', () => {
-		expectBindingToWork({
+		expectBindToPass({
 			initialValue: new Date('2023-01-01'),
 			updatedValue: new Date('2024-01-01'),
 			converterType: Object,
@@ -165,7 +153,7 @@ describe('BindDirective', () => {
 	})
 
 	describe('element', () => {
-		expectBindingToWork({
+		expectBindToPass({
 			initialValue: 'initial',
 			updatedValue: 'updated',
 			converterType: String,
