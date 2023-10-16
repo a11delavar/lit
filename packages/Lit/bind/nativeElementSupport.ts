446ecd3dc975294022a5e70679b9008bffa5832e
arshia11d <arshia11d@outlook.com>
2023-10-16 04:18:37 +0200

    Fix native element support

--- packages/Lit/bind/nativeElementSupport.ts
+++ packages/Lit/bind/nativeElementSupport.ts
@@ -1,4 +1,4 @@
 import { bindingDefaultPropertyKey } from './bindingDefaultProperty.js'
 
 const key = bindingDefaultPropertyKey;
-(HTMLInputElement as any)[key] = (HTMLInputElement as any)[key] = (HTMLInputElement as any)[key] = (HTMLInputElement as any)[key] = 'value'
+(HTMLInputElement as any)[key] = (HTMLTextAreaElement as any)[key] = (HTMLSelectElement as any)[key] = (HTMLProgressElement as any)[key] = 'value'
