851e4753e1f384607659e8dde30ee98d94aa9624
arshia11d <arshia11d@outlook.com>
2023-12-12 01:06:50 +0100

    chore(Lit): Make "bind" server-side

--- packages/Lit/bind/nativeElementSupport.ts
+++ packages/Lit/bind/nativeElementSupport.ts
@@ -1,4 +1,7 @@
+import { isServer } from 'lit'
 import { bindingDefaultPropertyKey } from './bindingDefaultProperty.js'
 
-const key = bindingDefaultPropertyKey;
-(HTMLInputElement as any)[key] = (HTMLTextAreaElement as any)[key] = (HTMLSelectElement as any)[key] = (HTMLProgressElement as any)[key] = 'value'
+if (isServer === false) {
+	const key = bindingDefaultPropertyKey;
+	(HTMLInputElement as any)[key] = (HTMLTextAreaElement as any)[key] = (HTMLSelectElement as any)[key] = (HTMLProgressElement as any)[key] = 'value'
+}
